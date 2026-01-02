af03945e5b165a554e5ebbbc25d3affedfa176ec
Niloy Ghosh <ghosh.niloy@gmail.com>
2026-01-02 10:27:21 +0530

    Add training script with tokenizer utility
    Introduces train.py, which includes functions to build or load a WordLevel tokenizer for a given language using a dataset. Updates requirements.txt to include necessary dependencies.

---
+++ train.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+from pathlib import Path
+
+
+def get_all_sentences(dataset, lang):
+        for item in dataset:
+            yield item['translation'][lang]
+
+
+# Get or build tokenizer
+def get_or_build_tokenizer(config, dataset, lang):
+    # config['tokenizer_file'] = '../tokenizers/tokenizer_{0}.json'
+    tokenizer_path = Path(config['tokenizer_file'].format(lang=lang))
+
+    if not Path.exists(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
+        tokenizer.train_from_iterator(get_all_sentences(dataset, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+
+    return tokenizer
+
